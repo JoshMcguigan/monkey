71008ae5332d46437803e3184f3fed9c0f516a2f
Josh Mcguigan <joshmcg88@gmail.com>
2018-12-17 04:53:49 -0700

    parsing expression statement

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4,25 +4,28 @@ use crate::lexer::{Token, lexer};
 enum Statement {
     Let{ name: String, value: Expr},
     Return{ value: Expr },
+    Expression(Expr),
 }
 
 #[derive(Debug, PartialEq)]
 enum Expr {
     Const(u32),
+    Ident(String),
 }
 
 fn parse(mut input: Vec<Token>) -> Vec<Statement> {
     let mut program = vec![];
 
     loop {
-        let token = input.remove(0);
+        let token = &input[0];
 
         match token {
             Token::EOF => break,
             Token::LET => parse_let(&mut input, &mut program),
             Token::RETURN => parse_return(&mut input, &mut program),
-            _ => unimplemented!()
+            _ => program.push(Statement::Expression(parse_expression(&mut input)))
         }
+        assert_eq!(Token::SEMICOLON, input.remove(0));
 
     }
 
@@ -30,26 +33,31 @@ fn parse(mut input: Vec<Token>) -> Vec<Statement> {
 }
 
 fn parse_let(input: &mut Vec<Token>, program: &mut Vec<Statement>) {
+    assert_eq!(Token::LET, input.remove(0));
     let name = match input.remove(0) {
         Token::IDENT(name) => name,
         _ => panic!("parse error at let statement"),
     };
     assert_eq!(Token::ASSIGN, input.remove(0));
-    let value = match input.remove(0) {
-        Token::INT(value) => value,
-        _ => panic!("parse error at let statement"),
-    };
-    assert_eq!(Token::SEMICOLON, input.remove(0));
-    program.push(Statement::Let {name, value: Expr::Const(value)});
+    let value = parse_expression(input);
+    program.push(Statement::Let {name, value});
 }
 
 fn parse_return(input: &mut Vec<Token>, program: &mut Vec<Statement>) {
+    assert_eq!(Token::RETURN, input.remove(0));
     let value = match input.remove(0) {
         Token::INT(value) => value,
         _ => panic!("parse error at return statement"),
     };
-    assert_eq!(Token::SEMICOLON, input.remove(0));
     program.push(Statement::Return {value: Expr::Const(value)});
+}
+
+fn parse_expression(input: &mut Vec<Token>) -> Expr {
+    match input.remove(0) {
+        Token::INT(value) => Expr::Const(value),
+        Token::IDENT(value) => Expr::Ident(value),
+        _ => panic!("parse error at let statement"),
+    }
 }
 
 #[cfg(test)]
@@ -79,6 +87,34 @@ mod tests {
         assert_eq!(
             vec![
                 Statement::Return { value: Expr::Const(5) },
+            ],
+            ast
+        );
+    }
+
+    #[test]
+    fn parse_let_ident() {
+        let input = "let myVar = anotherV;";
+        let tokens = lexer().parse(input.as_bytes()).unwrap();
+        let ast = parse(tokens);
+
+        assert_eq!(
+            vec![
+                Statement::Let { name: String::from("myVar"), value: Expr::Ident(String::from("anotherV")) },
+            ],
+            ast
+        );
+    }
+
+    #[test]
+    fn parse_expression_statement() {
+        let input = "foo;";
+        let tokens = lexer().parse(input.as_bytes()).unwrap();
+        let ast = parse(tokens);
+
+        assert_eq!(
+            vec![
+                Statement::Expression(Expr::Ident(String::from("foo"))),
             ],
             ast
         );
