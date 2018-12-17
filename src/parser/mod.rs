83511504312b91c8d80ff240f88024c594178c6e
Josh Mcguigan <joshmcg88@gmail.com>
2018-12-17 05:56:23 -0700

    parsing infix expression

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -27,7 +27,16 @@ enum Operator {
     Minus,
 }
 
-
+#[derive(PartialOrd, PartialEq)]
+enum Precedence {
+    Lowest,
+    Equals ,     // ==
+    LessGreater, // > or <
+    Sum,         // +
+    Product,     // *
+    Prefix,      // -X or !X
+    Call,        // myFunction(X)
+}
 
 fn parse(mut input: Vec<Token>) -> Vec<Statement> {
     let mut program = vec![];
@@ -39,7 +48,11 @@ fn parse(mut input: Vec<Token>) -> Vec<Statement> {
             Token::EOF => break,
             Token::LET => parse_let(&mut input, &mut program),
             Token::RETURN => parse_return(&mut input, &mut program),
-            _ => program.push(Statement::Expression(parse_expression(&mut input)))
+            _ => program.push(
+                Statement::Expression(
+                    parse_expression(&mut input, Precedence::Lowest)
+                )
+            )
         }
         assert_eq!(Token::SEMICOLON, input.remove(0));
 
@@ -55,7 +68,7 @@ fn parse_let(input: &mut Vec<Token>, program: &mut Vec<Statement>) {
         _ => panic!("parse error at let statement"),
     };
     assert_eq!(Token::ASSIGN, input.remove(0));
-    let value = parse_expression(input);
+    let value = parse_expression(input, Precedence::Lowest);
     program.push(Statement::Let {name, value});
 }
 
@@ -68,19 +81,63 @@ fn parse_return(input: &mut Vec<Token>, program: &mut Vec<Statement>) {
     program.push(Statement::Return {value: Expr::Const(value)});
 }
 
-fn parse_expression(input: &mut Vec<Token>) -> Expr {
-    match input.remove(0) {
+fn parse_expression(input: &mut Vec<Token>, precedence: Precedence) -> Expr {
+    let mut left_expr = match input.remove(0) {
         Token::INT(value) => Expr::Const(value),
         Token::IDENT(value) => Expr::Ident(value),
         Token::BANG => Expr::Prefix{
             prefix: Prefix::Bang,
-            value: Box::new(parse_expression(input))
+            value: Box::new(parse_expression(input, Precedence::Prefix))
         },
         Token::MINUS => Expr::Prefix{
             prefix: Prefix::Minus,
-            value: Box::new(parse_expression(input))
+            value: Box::new(parse_expression(input, Precedence::Prefix))
         },
         _ => panic!("parse error at expression"),
+    };
+
+    let mut next_token = &input[0];
+    while precedence < next_token.precedence() {
+        left_expr = parse_infix(left_expr, input);
+        next_token = &input[0];
+    }
+
+    left_expr
+}
+
+fn parse_infix(left: Expr, input: &mut Vec<Token>) -> Expr {
+    let next_token = input.remove(0);
+    let operator = match &next_token {
+        Token::PLUS => Operator::Plus,
+        Token::MINUS => Operator::Minus,
+//        Token::SLASH => {},
+//        Token::ASTERISK => {},
+//        Token::LT => {},
+//        Token::GT => {},
+//        Token::EQ => {},
+//        Token::NOT_EQ => {},
+        _ => panic!("parse infix called on invalid operator"),
+    };
+    Expr::Infix {
+        left: Box::new(left),
+        operator,
+        right: Box::new(parse_expression(input, next_token.precedence())),
+    }
+}
+
+impl Token {
+    fn precedence(&self) -> Precedence {
+        match self {
+            Token::PLUS => Precedence::Sum,
+            Token::MINUS => Precedence::Sum,
+            Token::SLASH => Precedence::Product,
+            Token::ASTERISK => Precedence::Product,
+            Token::LT => Precedence::LessGreater,
+            Token::GT => Precedence::LessGreater,
+            Token::EQ => Precedence::Equals,
+            Token::NOT_EQ => Precedence::Equals,
+            _ => Precedence::Lowest
+        }
     }
 }
 
@@ -178,6 +235,29 @@ mod tests {
                         value: Box::new(Expr::Const(15))
                     }
                 ),
+            ],
+            ast
+        );
+    }
+
+    #[test]
+    fn precedence() {
+        assert!(Precedence::Lowest < Precedence::Call);
+    }
+
+    #[test]
+    fn parse_infix_expression() {
+        let input = "5 + 6;";
+        let tokens = lexer().parse(input.as_bytes()).unwrap();
+        let ast = parse(tokens);
+
+        assert_eq!(
+            vec![
+                Statement::Expression(Expr::Infix{
+                    left: Box::new(Expr::Const(5)),
+                    operator: Operator::Plus,
+                    right: Box::new(Expr::Const(6)),
+                }),
             ],
             ast
         );
