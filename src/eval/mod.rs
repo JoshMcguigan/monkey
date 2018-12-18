d57bb28d6d3f5e92e8b21fdfc6d0462795fcf2bb
Josh Mcguigan <joshmcg88@gmail.com>
2018-12-18 10:07:43 -0700

    eval infix operators

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,6 +1,7 @@
 use crate::parser::Statement;
 use crate::parser::Expr;
 use crate::parser::Prefix;
+use crate::parser::Operator;
 
 #[derive(Debug, PartialEq)]
 pub enum Object {
@@ -23,6 +24,56 @@ fn eval_expr(expression: Expr) -> Object {
             match *expr {
                 Expr::Const(val) => Object::Integer(-val),
                 _ => panic!("minus operator only valid for integer type"),
+            }
+        },
+        Expr::Infix { left, operator: Operator::Plus, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left + right),
+                _ => panic!("plus operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::Minus, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left - right),
+                _ => panic!("minus operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::Multiply, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left * right),
+                _ => panic!("multiply operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::Divide, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left / right),
+                _ => panic!("divide operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::LessThan, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left < right),
+                _ => panic!("less than operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::GreaterThan, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left > right),
+                _ => panic!("greater than operator only valid on integer types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::Equals, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left == right),
+                (Expr::Boolean(left), Expr::Boolean(right)) => Object::Boolean(left == right),
+                _ => panic!("equals operator used on invalid types")
+            }
+        },
+        Expr::Infix { left, operator: Operator::NotEquals, right } => {
+            match (*left, *right) {
+                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left != right),
+                (Expr::Boolean(left), Expr::Boolean(right)) => Object::Boolean(left != right),
+                _ => panic!("not equals operator used on invalid types")
             }
         },
         _ => panic!("eval expr not implemented for this type")
@@ -68,6 +119,21 @@ mod tests {
     #[test]
     fn eval_negative() {
         test_eval("-5;", Object::Integer(-5));
+    }
+
+    #[test]
+    fn eval_infix() {
+        test_eval("5 + 5;", Object::Integer(10));
+        test_eval("5 - 5;", Object::Integer(0));
+        test_eval("5 * 5;", Object::Integer(25));
+        test_eval("5 / 5;", Object::Integer(1));
+        test_eval("5 > 1;", Object::Boolean(true));
+        test_eval("5 < 1;", Object::Boolean(false));
+        test_eval("5 == 1;", Object::Boolean(false));
+        test_eval("5 != 1;", Object::Boolean(true));
+        test_eval("true == true;", Object::Boolean(true));
+        test_eval("true != true;", Object::Boolean(false));
+//        test_eval("(1 > 2) == false;", Object::Boolean(true));
     }
 
     fn test_eval(input: &str, expected: Object) {
