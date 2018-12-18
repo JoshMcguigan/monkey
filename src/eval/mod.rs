4d66da57b0ca66a7e465b356a24769ba535c13ab
Josh Mcguigan <joshmcg88@gmail.com>
2018-12-18 13:12:59 -0700

    eval nested types

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -15,64 +15,64 @@ fn eval_expr(expression: Expr) -> Object {
         Expr::Const(num) => Object::Integer(num),
         Expr::Boolean(val) => Object::Boolean(val),
         Expr::Prefix { prefix: Prefix::Bang, value: expr } => {
-            match *expr {
-                Expr::Boolean(val) => Object::Boolean(!val),
+            match eval_expr(*expr) {
+                Object::Boolean(val) => Object::Boolean(!val),
                 _ => panic!("! operator only valid for boolean type"),
             }
         },
         Expr::Prefix { prefix: Prefix::Minus, value: expr } => {
-            match *expr {
-                Expr::Const(val) => Object::Integer(-val),
+            match eval_expr(*expr) {
+                Object::Integer(val) => Object::Integer(-val),
                 _ => panic!("minus operator only valid for integer type"),
             }
         },
         Expr::Infix { left, operator: Operator::Plus, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left + right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Integer(left + right),
                 _ => panic!("plus operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::Minus, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left - right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Integer(left - right),
                 _ => panic!("minus operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::Multiply, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left * right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Integer(left * right),
                 _ => panic!("multiply operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::Divide, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Integer(left / right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Integer(left / right),
                 _ => panic!("divide operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::LessThan, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left < right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Boolean(left < right),
                 _ => panic!("less than operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::GreaterThan, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left > right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Boolean(left > right),
                 _ => panic!("greater than operator only valid on integer types")
             }
         },
         Expr::Infix { left, operator: Operator::Equals, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left == right),
-                (Expr::Boolean(left), Expr::Boolean(right)) => Object::Boolean(left == right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Boolean(left == right),
+                (Object::Boolean(left), Object::Boolean(right)) => Object::Boolean(left == right),
                 _ => panic!("equals operator used on invalid types")
             }
         },
         Expr::Infix { left, operator: Operator::NotEquals, right } => {
-            match (*left, *right) {
-                (Expr::Const(left), Expr::Const(right)) => Object::Boolean(left != right),
-                (Expr::Boolean(left), Expr::Boolean(right)) => Object::Boolean(left != right),
+            match (eval_expr(*left), eval_expr(*right)) {
+                (Object::Integer(left), Object::Integer(right)) => Object::Boolean(left != right),
+                (Object::Boolean(left), Object::Boolean(right)) => Object::Boolean(left != right),
                 _ => panic!("not equals operator used on invalid types")
             }
         },
@@ -114,11 +114,13 @@ mod tests {
     fn eval_bang() {
         test_eval("!true;", Object::Boolean(false));
         test_eval("!false;", Object::Boolean(true));
+        test_eval("!(1 > 2);", Object::Boolean(true));
     }
 
     #[test]
     fn eval_negative() {
         test_eval("-5;", Object::Integer(-5));
+        test_eval("-(1 - 2);", Object::Integer(1));
     }
 
     #[test]
@@ -133,7 +135,19 @@ mod tests {
         test_eval("5 != 1;", Object::Boolean(true));
         test_eval("true == true;", Object::Boolean(true));
         test_eval("true != true;", Object::Boolean(false));
-//        test_eval("(1 > 2) == false;", Object::Boolean(true));
+        test_eval("(1 > 2) == false;", Object::Boolean(true));
+    }
+
+    #[test]
+    fn eval_infix_nested_types() {
+        test_eval("(1 + 2) + 3;", Object::Integer(6));
+        test_eval("(1 + 2) - 3;", Object::Integer(0));
+        test_eval("(1 + 2) * 3;", Object::Integer(9));
+        test_eval("(1 + 2) / 3;", Object::Integer(1));
+        test_eval("(1 + 2) < 3;", Object::Boolean(false));
+        test_eval("(1 + 2) > 3;", Object::Boolean(false));
+        test_eval("(1 > 2) == false;", Object::Boolean(true));
+        test_eval("(1 > 2) != false;", Object::Boolean(false));
     }
 
     fn test_eval(input: &str, expected: Object) {
